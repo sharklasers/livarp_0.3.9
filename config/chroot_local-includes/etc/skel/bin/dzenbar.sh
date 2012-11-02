cddc13872bcc696def5bef8d1eedb779a65fd5a2
arnault <contact@arpinux.org>
2012-11-02 15:55:20 +0100

    fix config files and stuff

--- config/chroot_local-includes/etc/skel/bin/dzenbar.sh
+++ config/chroot_local-includes/etc/skel/bin/dzenbar.sh
@@ -8,7 +8,7 @@ HEIGHT="12"
 FONT="-*-terminus-medium-*-*-*-12-*-*-*-*-*-*-*"
 XPOS="0"
 YPOS="0"
-LIN="8" #number of lines in conky_dzen output
+LIN="22" #number of lines in conky_dzen output
 
-conky -d -c $RC | dzen2 -e 'button1=togglecollapse;button2=exec:$HOME/bin/dzenbar_reload.sh;button3=exec:geany $HOME/.conky/.conkyrc_dzen $HOME/bin/dzenbar.sh' -fg $FG -bg $BG -ta $ALIGN -h $HEIGHT -x $XPOS -y $YPOS -fn $FONT -l $LIN -u &
+conky -c $RC | dzen2 -e 'button1=togglecollapse;button2=exec:$HOME/bin/dzenbar_reload.sh;button3=exec:geany $HOME/.conky/.conkyrc_dzen $HOME/bin/dzenbar.sh' -fg $FG -bg $BG -ta $ALIGN -h $HEIGHT -x $XPOS -y $YPOS -fn $FONT -l $LIN -u &
 exit 0
